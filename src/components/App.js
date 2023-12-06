7be0486b47076e3ad6d32a0cba5466360b4239db
Shkliarova <shklyarovaalisa@gmail.com>
2023-12-06 22:18:16 +0200

    hw part1

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import HomePage from "pages/HomePage"
+import MoviePage from "pages/MoviesPage"
+import MovieDetailsPage from "pages/MovieDetailsPage"
+import NotFoundPage from "pages/NotFoundPage"
+import { AppLayout } from "./AppLayout"
+import { Cast } from "./Cast"
+import { Reviews } from "./Reviews"
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={ <AppLayout/> }>
+        <Route index element={ <HomePage/> } />
+        <Route path="/movies" element={ <MoviePage/> } />
+        <Route path="/movies/:movieId" element={ <MovieDetailsPage/> }>
+          <Route path="cast" element={ <Cast/> } />
+          <Route path="reviews" element={ <Reviews/> } />
+        </Route>
+      </Route>
+      <Route path="*" element={ <NotFoundPage/> } />
+    </Routes>
+  )
+}
